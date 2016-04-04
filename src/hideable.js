cf6637fddf7e70d40923c884d8358addc3360631
Adam Donahue <adonahue@squarespace.com>
2016-04-04 14:04:36 -0400

    Some minor cleanup.

--- src/hideable.js
+++ src/hideable.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Hideable = ({children, isHidden=false}) => {
   if (isHidden)
-    return <div />;
+    return <div/>;
   return <div>{children}</div>;
 };
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Hideable from './hideable';
 
-export default {
+export {
   Hideable
 };
